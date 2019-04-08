43d186177094fc2d37dcf5a835339278b983433f
Ryan Haig <haigryan@gmail.com>
2019-04-07 21:03:55 -0600

    Initialized Profile schema

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -29,4 +29,70 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
         .catch(err => res.status(404).json(err))
 })
 
+// @route   POST /api/profile
+// @desc    Create or edit User profile
+// @access  Private
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    // Get Fields
+    const profileFields = {...req.body}
+
+    //console.log( JSON.stringify(profileFields, undefined, 2) )
+
+    const errors = {}
+
+    // Required fields
+    if(!profileFields.handle) errors.handle = 'Handle Required'
+    if(!profileFields.status) errors.status = 'Status Required'
+    if(!profileFields.skills) errors.skills = 'Skills Required'
+
+    // Skills array
+    if(typeof skills !== 'undefined') {
+        profileFields.skills =  profileFields.skills.split(',')
+    }
+
+    // Social, weird because it's an object
+    profileFields.social = {}
+    if(req.body.youtube) profileFields.social.youtube = req.body.youtube
+    if(req.body.twitter) profileFields.social.twitter = req.body.twitter
+    if(req.body.linkedin) profileFields.social.linkedin = req.body.linkedin
+    if(req.body.facebook) profileFields.social.facebook = req.body.facebook
+    if(req.body.instagram) profileFields.social.instagram = req.body.instagram
+
+    Profile.findOne({ user: req.user.id})
+        .then(profile => {
+            if(profile){
+                // Update Profile
+                Profile.findOneAndUpdate(
+                    { user: req.user.id}, 
+                    { $set: profileFields }, 
+                    { new: true}
+                )
+                .then(profile => res.json(profile))
+            } else {
+                // Create Profile
+
+                // Check if handle exists
+                Profile.findOne({ handle: profileFields.handle })
+                    .then(profile => {
+                        if(profile){
+                            errors.handle = 'Handle already exists'
+                        }
+
+                        // if there's errors, throw errors with 412
+                        if(Object.keys(errors).length) {
+                            res.status(412).json(errors)
+                        } else {
+                            // Save Profile
+                            new Profile(profileFields)
+                            .save()
+                            .then(profile => res.json(profile))
+                            .catch(err => res.json(err))
+                        }
+                    })
+            }
+        })
+
+    
+})
+
 module.exports = router;
