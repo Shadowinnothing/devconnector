e7ee064e988a833e54d82cf69f2bcdac2ab7a0ad
Ryan Haig <haigryan@gmail.com>
2019-04-13 15:46:29 -0600

    Created POST method to create a new Post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,36 @@
 const router = require('express').Router()
+const mongoose = require('mongoose')
+const passport = require('passport')
 
-// @route   GET /api/posts/test
-// @desc    Test post route
-// @access  Public
-router.get('/test', (req, res) => { res.json({msg: "Posts is working"}) })
+// Post Modal
+const Post = require('../../models/Post')
+
+// Validation
+const validatePostInput = require('../../validation/post')
+
+// @route   POST /api/posts
+// @desc    Create Post
+// @access  Private
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body)
+
+    // Check Validation
+    if(!isValid){
+        return res.status(400).json(errors)
+    }
+
+    const { text, name, avatar, user } = req.body
+    const newPost = new Post({
+        text,
+        name,
+        avatar,
+        user
+    })
+
+    newPost
+        .save()
+        .then(post => res.json(post))
+        .catch(err => res.json(err))
+})
 
 module.exports = router;
---
+++ validation/post.js
@@ -0,0 +1,20 @@
+const Validator = require('Validator')
+
+const isEmpty = require('./is-empty')
+
+module.exports = validatePostInput = (data) => {
+    let errors = {}
+
+    // Check to make sure that name/email/password are not empty
+    data.text = !isEmpty(data.text) ? data.text : ''
+
+    if(!Validator.isLength(data.text, { min: 10, max: 300 })) {
+        errors.text = 'Post must be between 10 and 300 chars'
+    }
+
+    if(Validator.isEmpty(data.text)) {
+        errors.text = 'Post text is invalid'
+    }
+
+    return{ errors, isValid: isEmpty(errors) }
+}
