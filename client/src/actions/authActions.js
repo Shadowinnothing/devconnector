47d2a8c550ca946b771205b6c33e255beacc2012
Ryan Haig <haigryan@gmail.com>
2019-04-28 22:43:47 -0600

    Added first layer of user login functionality

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
-import { GET_ERRORS } from './types'
+import jwt_decode from 'jwt-decode'
+
+import setAuthToken from '../utils/setAuthToken'
+import { GET_ERRORS, SET_CURRENT_USER } from './types'
 
 // Register a user
 export const registerUser = (userData, history) => dispatch => {
@@ -11,4 +14,33 @@ export const registerUser = (userData, history) => dispatch => {
                 payload: err.response.data
             })
         )
+}
+
+// Login User - Get user Token
+export const loginUser = userData => dispatch => {
+    axios.post('/api/users/login', userData)
+        .then(res => {
+            // Save to localStorage, set to auth header
+            const { token } = res.data
+            localStorage.setItem('jwtToken', token)
+            setAuthToken(token)
+            
+            // Decode token to get user data
+            const decodedUserData = jwt_decode(token)
+            dispatch(setCurrentUser(decodedUserData))
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        })
+}
+
+// Set logged in user
+export const setCurrentUser = decodedUserData => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decodedUserData
+    }
 }
